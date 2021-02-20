a91211e936aee35c14a8539a1b7a3edfc987114f
Alexey Tkachuk <sektor_les@mail.ru>
2021-02-20 18:56:12 +0300

    Almost

---
+++ AddictForMatrix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComplainMatrix
+{
+    public static class AddictForMatrix
+    {
+        public static int[,] AddictionMatrix(this int[,] matrixNumberOne, int[,] matrixNumberTwo)
+        {
+            int a = 0;
+            int b = 0;
+
+            int[,] addictionedMatrix = null;
+            for (int i = 0; i < matrixNumberOne.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixNumberOne.GetLength(1); j++)
+                {
+                    if (matrixNumberOne[i, j] == null) matrixNumberOne[i, j] = 0;
+                    if (matrixNumberTwo[i, j] == null) matrixNumberTwo[i, j] = 0;
+                    if (i == j) addictionedMatrix[a, b] = matrixNumberOne[i, j] + matrixNumberTwo[i, j];
+                }
+            }
+            return addictionedMatrix;
+        }
+    }
+}
--- Matrix.cs
+++ Matrix.cs
@@ -4,12 +4,15 @@ using System.Text;
 
 namespace SecondHomeTask
 {
-    class Matrix
+    public class Matrix
     {
         private int[,] matrix;
+        private int size;
         public int Size { get; }
         public Matrix(params int[] diagonal)
         {
+            size = diagonal.Length;
+            Size = size;
             matrix = new int[diagonal.Length, diagonal.Length];
             for (int i = 0 ; i < diagonal.Length; i++)
             {
@@ -18,8 +21,8 @@ namespace SecondHomeTask
                     if (i == j) matrix[i,j] = diagonal[i];
                 }
             }
-            if (diagonal == null || diagonal.Length < 1) Size = 0;
-            else Size = diagonal.Length;
+            if (diagonal == null || diagonal.Length < 1) size = 0;
+            else size = diagonal.Length;
         }
 
         public int this[int i, int j]
@@ -38,22 +41,44 @@ namespace SecondHomeTask
         public int Track ()
         {
             int result = 0;
-            for (int i = 0; i < Size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < Size; j++)
+                for (int j = 0; j < size; j++)
                 {
                     if (i == j)
                     {
                         result += matrix[i, j];
                     }
                 }
-                
             }
             return result;
         }
         public override string ToString()
         {
-            return base.ToString();
+            string result = string.Empty;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    result += matrix[i, j] + " ";
+                }
+                result +="\n";
+            }
+            return result;
         }
+        public bool Equals(int[,] firstMatrix, int[,] secondMatrix)
+        {
+            if (firstMatrix.Length != secondMatrix.Length) return false;
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (firstMatrix[i, j] != secondMatrix[i, j]) return false;
+                }
+            }
+            return true;
+        }
+       
     }
 }
